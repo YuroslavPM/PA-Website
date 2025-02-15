ab547f9ea0dfe4923ffb89f3ef2a9b0d12e9743f
YuroslavPM <yuroslavpm11@gmail.com>
2025-02-15 12:38:36 +0200

    Create model Service, modified all views (Service),

---
+++ PA Website/PA Website/Models/Service.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PA_Website.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NameService { get; set; }
+        [Required]
+        public string CategoryOfService { get; set; }
+        
+        public DateTime ReservationDate { get; set; }
+
+        public string Description { get; set; } 
+
+
+
+    }
+}
